a03343b15c6c79afeb1896fede1698a31bab8041
Toby Kennon <tobykennon@googlemail.com>
2020-05-29 10:40:00 +0100

    updates to godoc

--- ticker.go
+++ ticker.go
@@ -56,11 +56,11 @@ func newTicker(interval interval) *Ticker {
 }
 
 // WithJitter adds a uniformly random jitter to the time the ticker next fires.
-// The jitter will be within `fraction` of the current ticker. e.g. if
-// WithJitter(0.2) is applied to an exponential ticker that would otherwise fire
-// on 1, 2, 4, 8, ... seconds then the first ticker will fire in between 0.8-1.2
-// seconds, and the second will fire between 1.6-2.4 seconds. The jitter
-// fraction may be greater than one, allowing the possiblity for jittered
+// The jitter will be within `fraction` of the next interval. For example,
+// WithJitter(0.1) applies a 10% jitter, so a linear ticker that would otherwise
+// fire after 1, 2, 3, 4, ... second would now fire between 0.9-1.1 seconds on
+// the first tick, and 1.8-2.2 seconds on the second tick, and so forth. The
+// jitter fraction may be greater than one, allowing the possiblity for jittered
 // tickers to fire immediately if the calculated interval with the jitter is
 // less than zero.
 func (t *Ticker) WithJitter(fraction float64) *Ticker {
@@ -84,9 +84,9 @@ func (t *Ticker) WithMaxInterval(d time.Duration) *Ticker {
 	return t
 }
 
-// WithMaxDuration sets the maxiumum cumulative total duration the ticker will
-// run for. Once the maximum duration is reached, the ticker's channel will be
-// closed.
+// WithMaxDuration sets the maxiumum total duration over all ticks that the
+// ticker will run for. Once the maximum duration is reached, the ticker's
+// channel will be closed.
 func (t *Ticker) WithMaxDuration(d time.Duration) *Ticker {
 	t.maxDuration = d
 	t.maxDurationSet = true
@@ -94,7 +94,7 @@ func (t *Ticker) WithMaxDuration(d time.Duration) *Ticker {
 }
 
 // WithContext adds a context.Context to the ticker. If the context expires then
-// the tickers channel will be closed.
+// the ticker's channel will be closed.
 func (t *Ticker) WithContext(ctx context.Context) *Ticker {
 	t.ctx = ctx
 	return t
@@ -110,7 +110,7 @@ func (t *Ticker) WithFunc(f func()) *Ticker {
 // Start starts the ticker. The ticker's channel will send the current time
 // after an interval has elapsed. Start returns an error if the ticker could not
 // be started due to restrictions imposed in the ticker config (e.g. minimum
-// allows interval is greater than the maximum allowed interval). The ticker's
+// allowed interval is greater than the maximum allowed interval). The ticker's
 // channel will fire at different intervals determined by the type of ticker
 // (e.g. constant, linear, or exponential) and by the ticker modifiers
 // (WithJitter, WithMaximum etc). The ticker must be stopped (either explicitly
